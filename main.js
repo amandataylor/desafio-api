718cc5887786e3ba157ddea32fd1c03d5ff087e6
Amanda Taylor <amanda.taylor@hotelurbano.com.br>
2016-03-04 11:15:08 -0300

    Testes de Olá mundo

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-React.render(
-  React.DOM.h1(null, "Hello world!"),
-  document.getElementById("app")
-);
+// Importando o módulo express e criando um app.
+var express = require('express'),
+    app = express();
+
+// Estabelendo uma rota. Consiste em um método HTTP, um caminho e uma função callback
+// O “Ola Mundo” chama app.get() que representa o método GET do HTTP, e o caminho / representando a página raiz, seguido da função de callback.
+app.get('/', function(req, res){
+    res.send('Olá Mundo!');
+});
+
+// Informando o servidor para escutar uma porta.
+app.listen(3001, 'localhost', function() {
+  console.log("... port %d in %s mode", app.address().port, app.settings.env);
+});
